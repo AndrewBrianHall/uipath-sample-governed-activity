aebc05d23460ad8336b5817c283a70541e323b9c
Andrew Hall <andrew.hall@uipath.com>
2020-11-13 10:43:44 -0500

    fix pack to use configuration

--- build/nuget_pack.cmd
+++ build/nuget_pack.cmd
@@ -5,4 +5,4 @@ IF /I "%1" == "-release" SET _configuration=Release
 SET _build_dir=%_src_root%\bin\%_configuration%
 
 msbuild %_src_root%\%_PROJECT_NAME%.csproj -p:Configuration=%_configuration%
-nuget pack %_src_root% -OutputDirectory %_build_dir%
+nuget pack %_src_root% -OutputDirectory %_build_dir% -p Configuration=%_configuration%
